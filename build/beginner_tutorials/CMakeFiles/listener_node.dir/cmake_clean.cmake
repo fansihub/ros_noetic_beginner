d62f81a98a5d7d72139b4a0d74feb78208110646
fansi <fansinmn@outlook.com>
2023-10-18 22:32:45 +0800

    init

---
+++ build/beginner_tutorials/CMakeFiles/listener_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/fs/catkin_ws/devel/lib/beginner_tutorials/listener_node"
+  "/home/fs/catkin_ws/devel/lib/beginner_tutorials/listener_node.pdb"
+  "CMakeFiles/listener_node.dir/src/listener_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/beginner_tutorials/CMakeFiles/talker_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/fs/catkin_ws/devel/lib/beginner_tutorials/talker_node"
+  "/home/fs/catkin_ws/devel/lib/beginner_tutorials/talker_node.pdb"
+  "CMakeFiles/talker_node.dir/src/talker_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
