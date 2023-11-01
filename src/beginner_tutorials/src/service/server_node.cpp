b30b81101285ec0c06f16766424a5bfe4b0eee53
fansi_wsl <ubuntu2004_wsl@example.com>
2023-11-01 21:21:16 +0800

    调整目录结构

---
+++ src/beginner_tutorials/src/service/server_node.cpp
@@ -0,0 +1,28 @@
+// 搬运自src/ros_tutorials/roscpp_tutorials/add_two_ints_server/add_two_ints_server.cpp
+
+#include "ros/ros.h"
+#include "roscpp_tutorials/TwoInts.h"
+
+bool add(roscpp_tutorials::TwoInts::Request  &req,
+         roscpp_tutorials::TwoInts::Response &res )
+{
+  res.sum = req.a + req.b;
+  ROS_INFO("request: x=%ld, y=%ld", (long int)req.a, (long int)req.b);
+  ROS_INFO("  sending back response: [%ld]", (long int)res.sum);
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "add_two_ints_server");
+  ros::NodeHandle n;
+
+// %Tag(SERVICE_SERVER)%
+  ros::ServiceServer service = n.advertiseService("add_two_ints", add);
+// %EndTag(SERVICE_SERVER)%
+
+  ros::spin();
+
+  return 0;
+}
+
